2a01ffdc32c30c4599f37334cd28b6ac4cdd95ec
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-09-03 14:57:11 +0900

    prettier

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,12 +1,12 @@
-import { setUserCookie } from "../../lib/userCookies"
+import { setUserCookie } from "../../lib/userCookies";
 
 beforeEach(() => {
-  setUserCookie({id: "123", email: "abc@example.com"})
-})
+  setUserCookie({ id: "123", email: "abc@example.com" });
+});
 
-describe('Index', () => {
-  it('Show page', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Okini')
-  })
-})
+describe("Index", () => {
+  it("Show page", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Okini");
+  });
+});
